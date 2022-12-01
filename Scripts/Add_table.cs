7ce6e5abb5795cf1a94b49545bf2fa5b2d1239c0
larryfans <59216760+larryfans@users.noreply.github.com>
2022-12-01 11:24:47 +0800

    Add files via upload
    code of connect

---
+++ Scripts/Add_table.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using UnityEngine;
+
+public class Add_table : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SqlAccess sql = new SqlAccess();
+        string[] items = { "id", "name" };//更改元素，与自己表对应
+        
+        string[] val = {"36", "'smart'"};   //需要添加的信息
+        sql.Add_data("test_table1", items, val); ////读取表，此处把表名改为自己需要查询的表
+        DataSet ds = sql.SelectWhere("test_table1", items);//读取表，此处把表名改为自己需要查询的表
+
+        if (ds != null)
+        {
+            DataTable table = ds.Tables[0];
+            foreach (DataRow row in table.Rows)
+            {
+                string str = "";
+                foreach (DataColumn column in table.Columns)
+                    str += row[column] + " ";
+                Debug.Log(str);
+            }
+        }
+        sql.Close();
+
+    }
+
+}
---
+++ Scripts/test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+using System;
+
+public class test : MonoBehaviour
+{
+    // mysql连接字符串
+    static String ConnetStr = "Server=3.92.135.74;Database=mysql_test;UserID=root;Password=Haisec2022??;Port=3306;";
+    MySqlConnection conn = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 创建连接
+        conn = new MySqlConnection(ConnetStr);
+        try
+        {
+            conn.Open();    // 打开通道,建立连接
+            Debug.Log("连接成功...");
+            // 增、删、改、查的操作
+        }
+        catch (MySqlException ex)
+        {
+            // 打印异常信息
+            Debug.Log(ex.Message);
+        }
+        finally
+        {
+            // 关闭连接
+            if (conn != null)
+                conn.Close();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
